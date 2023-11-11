cd8f5adedbd1f3b0cdbd26f74b44734a162aaf86
jen <jboucher154@gmail.com>
2023-11-11 13:57:14 +0200

    set serial port and print decoded values from arduino

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from serial import Serial
 import time
 
-port = "/dev/something"
+port = "/dev/cu.usbmodem11201"
 arduino = Serial(port=port, baudrate=115200, timeout=.1)
 
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
-
-
 while True:
-    num = input("Enter a number: ")  # Taking input from user
-    value = write_read(num)
-    print(value)  # printing the value
+    # num = input("Enter a number: ")  # Taking input from user
+    value = arduino.readline()
+    print(value.decode("utf-8"))  # printing the value
