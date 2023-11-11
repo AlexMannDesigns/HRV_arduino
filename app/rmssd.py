6b708ef0a1090e674f93c90fc42a85d1fbe67d18
jen <jboucher154@gmail.com>
2023-11-11 18:54:55 +0200

    add rmssd non scaled

---
+++ app/rmssd.py
@@ -0,0 +1,16 @@
+
+import math
+
+
+def get_rmssd_score(deltas: list) -> float:
+    if len(deltas) < 50:
+        return 0
+    squares = []
+    for i in range(len(deltas)):
+        squares.append(deltas[i]**2)
+    mean = sum(squares) / len(squares)
+    root = math.sqrt(mean)
+    score = math.log(root)
+    print(score)
+    return score
+    # ln 0 - 6.5
